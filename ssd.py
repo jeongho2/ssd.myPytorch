33f2edc324b9df4f9d2f8de3cb1d300adce3406c
jeongho2 <jeongho.kim@snuailab.ai>
2021-12-16 19:50:56 +0900

    import raccon 주석

--- ssd.py
+++ ssd.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from layers import *
 from data import voc, coco
+# from data import voc, coco, raccoon
 import os
 
 
@@ -29,9 +30,12 @@ class SSD(nn.Module):
         super(SSD, self).__init__()
         self.phase = phase
         self.num_classes = num_classes
+        # self.cfg = (coco, voc, raccoon)[num_classes == 21]
         self.cfg = (coco, voc)[num_classes == 21]
         self.priorbox = PriorBox(self.cfg)
         self.priors = Variable(self.priorbox.forward(), volatile=True)
+        # with torch.no_grad():
+            # self.priors = self.priorbox.forward()
         self.size = size
 
         # SSD network
